54eb832d54a3ef4db54e19f66ac404fba4749696
yogamahottama <yoga.mahottama@gmail.com>
2020-05-09 21:16:28 +0800

    new

---
+++ src/components/views/Penelitian.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>The component will appear here</div>
+</template>
+<script>
+import axios from 'axios'
+var host = 'http://10.199.14.46:8028/'
+import WebDataRocks from 'webdatarocks'
+export default {
+  mounted: function() {
+    axios.get(host + 'api/Penelitian/').then(res => {
+      console.log(res.data)
+      this.webdatarocks = new WebDataRocks({
+        report: { data: res.data },
+        container: this.$el
+      })
+    })
+  },
+  beforeUpdate() {
+    return false
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '~webdatarocks/webdatarocks.min.css';
+</style>
